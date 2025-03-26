3056ced712585e8c44bafe9645ece6409b1853cb
Sviatoslav620 <sviatpoliak@gmail.com>
2025-03-26 10:30:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # Оновлення списку пакетів
-apt-get update
+apt-get update 
 
-# Встановлення необхідних залежностей
-apt-get install -y unzip wget
+# Встановлення необхідних утиліт
+apt-get install -y wget unzip
 
 # Завантаження та встановлення Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
-dpkg -i chrome.deb || apt-get install -fy
-rm chrome.deb
+wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y /tmp/chrome.deb
+rm /tmp/chrome.deb
 
-# Завантаження та встановлення ChromeDriver
+# Завантаження ChromeDriver
 CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip -O chromedriver.zip
-unzip -o chromedriver.zip -d /usr/local/bin/
-rm chromedriver.zip
-chmod +x /usr/local/bin/chromedriver
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+
+# Розпакування ChromeDriver у робочу папку
+unzip /tmp/chromedriver.zip -d /opt/render/project/src/
+rm /tmp/chromedriver.zip
+
+# Надаємо права на виконання
+chmod +x /opt/render/project/src/chromedriver
 
 # Запуск бота
 python bot.py
