fc6d8e89a5fa4a3600c6d26f3a10f048b745f208
Sviatoslav620 <sviatpoliak@gmail.com>
2025-03-26 10:26:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Надати файлу start.sh права на виконання
-chmod +x start.sh
+# Оновлення списку пакетів
+apt-get update
 
-# Завантажуємо Chrome
-echo "Downloading Chrome..."
+# Встановлення необхідних залежностей
+apt-get install -y unzip wget
+
+# Завантаження та встановлення Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
-sudo dpkg -i chrome.deb || sudo apt-get -f install -y
+dpkg -i chrome.deb || apt-get install -fy
 rm chrome.deb
 
-# Завантажуємо ChromeDriver
-echo "Downloading ChromeDriver..."
+# Завантаження та встановлення ChromeDriver
 CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-sudo mv chromedriver /usr/local/bin/
-rm chromedriver_linux64.zip
+wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip -O chromedriver.zip
+unzip -o chromedriver.zip -d /usr/local/bin/
+rm chromedriver.zip
+chmod +x /usr/local/bin/chromedriver
 
-# Запускаємо бота
-echo "Starting bot..."
+# Запуск бота
 python bot.py
