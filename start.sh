4ed00abb251eea26750a445340b955409762997a
Sviatoslav620 <sviatpoliak@gmail.com>
2025-03-26 15:00:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
 # Оновлення списку пакетів
-apt-get update
+apt-get update 
 
-# Завантаження та встановлення Firefox у доступну папку
-mkdir -p /tmp/firefox
-wget -O /tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
-tar xjf /tmp/firefox.tar.bz2 -C /tmp/firefox
-rm /tmp/firefox.tar.bz2
-export PATH="/tmp/firefox/firefox/:$PATH"
+# Встановлення Firefox
+apt-get install -y firefox
 
-# Завантаження та встановлення Geckodriver
-GECKODRIVER_VERSION=$(curl -sS https://github.com/mozilla/geckodriver/releases/latest | grep -oP 'v\d+\.\d+\.\d+')
-wget -O /tmp/geckodriver.tar.gz "https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-linux64.tar.gz"
-tar -xzf /tmp/geckodriver.tar.gz -C /tmp/firefox/
+# Завантаження GeckoDriver
+GECKODRIVER_VERSION=$(curl -sS https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
+wget -O /tmp/geckodriver.tar.gz "https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
+
+# Розпакування GeckoDriver у робочу папку
+mkdir -p /opt/firefox/
+tar -xzf /tmp/geckodriver.tar.gz -C /opt/firefox/
 rm /tmp/geckodriver.tar.gz
-chmod +x /tmp/firefox/geckodriver
-export PATH="/tmp/firefox/:$PATH"
 
-# Логування версій для перевірки
-echo "Firefox version:"
-/tmp/firefox/firefox --version
-
-echo "Geckodriver version:"
-/tmp/firefox/geckodriver --version
+# Надаємо права на виконання
+chmod +x /opt/firefox/geckodriver
 
 # Запуск бота
 python bot.py
